87b963f0152a5243bd743a44e5ec09f8e15ea4e8
bhatt-nish-0 <bhattnish0@gmail.com>
2024-08-25 19:09:08 +0800

    tes

---
+++ security/src/main/java/com/alibou/security/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.alibou.security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> getString() {
+        return ResponseEntity.ok("hello from secured app~~~");
+    }
+}
